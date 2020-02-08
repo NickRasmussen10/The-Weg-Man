8e4fb6c215bb9434f85d8c9c99d1e4ed81d4082f
Nick <nickras35@gmail.com>
2020-02-08 15:41:08 -0500

    temp fix to player movement

--- The Weg Man/Assets/Scripts/PlayerMovement.cs
+++ The Weg Man/Assets/Scripts/PlayerMovement.cs
@@ -6,75 +6,32 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
 
-    [SerializeField] List<Rigidbody> ragdollRigidBodies = new List<Rigidbody>();
+    [SerializeField] GameObject pref_ragdoll;
 
     [SerializeField] bool startRagdoll = false;
-
-    bool ragdoll = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Rigidbody rb in ragdollRigidBodies)
-        {
-            if (startRagdoll)
-            {
-                rb.freezeRotation = false;
-            }
-            else
-            {
-                rb.freezeRotation = true;
-            }
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
-        transform.Rotate(Vector3.down, Input.GetAxis("Horizontal"));
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        //transform.Rotate(Vector3.down, Input.GetAxis("Horizontal"));
 
         if (Input.GetButtonDown("Jump"))
         {
-            ragdoll = !ragdoll;
-            if (ragdoll)
-            {
-                StartCoroutine(Launch());
-            }
-        }
 
-
-
-        if (ragdoll && ragdollRigidBodies[0].freezeRotation)
-        {
-            foreach (Rigidbody rb in ragdollRigidBodies)
-            {
-                rb.freezeRotation = false;
-            }
-        }
-        else if(!ragdoll && !ragdollRigidBodies[0].freezeRotation)
-        {
-            foreach (Rigidbody rb in ragdollRigidBodies)
-            {
-                rb.freezeRotation = true;
-            }
         }
     }
 
 
-    IEnumerator Launch()
+   void Launch()
     {
-        foreach (Rigidbody rb in ragdollRigidBodies)
-        {
-            rb.AddForce(-transform.forward * Random.Range(-5.0f, 20.0f), ForceMode.Impulse);
-        }
-
-        yield return new WaitForSeconds(1.0f);
-
-        transform.position = ragdollRigidBodies[0].position;
-        foreach (Rigidbody rb in ragdollRigidBodies)
-        {
-            rb.position = Vector3.zero;
-        }
+        GameObject ragdoll = Instantiate(pref_ragdoll, transform.position, transform.rotation);
+        ragdoll.GetComponent<Ragdoll>().Launch();
     }
 }
---
+++ The Weg Man/Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField] List<Rigidbody> rbs = new List<Rigidbody>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Launch()
+    {
+        foreach (Rigidbody rb in rbs)
+        {
+            rb.AddForce(transform.forward * Random.Range(10.0f, 15.0f), ForceMode.Impulse);
+        }
+    }
+}
