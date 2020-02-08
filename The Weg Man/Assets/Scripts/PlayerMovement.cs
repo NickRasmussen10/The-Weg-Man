68bfd4aa7664849483cefeb976adc3583d856679
Nick <nickras35@gmail.com>
2020-02-08 13:49:54 -0500

    started player movement

---
+++ The Weg Man/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+
+    bool ragdoll = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ragdoll)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
