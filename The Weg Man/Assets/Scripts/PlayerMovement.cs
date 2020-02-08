38802f16d18059255a662dae20bd84b8ad52d1ce
Nick <nickras35@gmail.com>
2020-02-08 14:34:07 -0500

    changed player movement to launches, broke everything

--- The Weg Man/Assets/Scripts/PlayerMovement.cs
+++ The Weg Man/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,75 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
 
+    [SerializeField] List<Rigidbody> ragdollRigidBodies = new List<Rigidbody>();
+
+    [SerializeField] bool startRagdoll = false;
+
     bool ragdoll = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (ragdoll)
+        foreach (Rigidbody rb in ragdollRigidBodies)
         {
-
+            if (startRagdoll)
+            {
+                rb.freezeRotation = false;
+            }
+            else
+            {
+                rb.freezeRotation = true;
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0.0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Rotate(Vector3.down, Input.GetAxis("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            ragdoll = !ragdoll;
+            if (ragdoll)
+            {
+                StartCoroutine(Launch());
+            }
+        }
+
+
+
+        if (ragdoll && ragdollRigidBodies[0].freezeRotation)
+        {
+            foreach (Rigidbody rb in ragdollRigidBodies)
+            {
+                rb.freezeRotation = false;
+            }
+        }
+        else if(!ragdoll && !ragdollRigidBodies[0].freezeRotation)
+        {
+            foreach (Rigidbody rb in ragdollRigidBodies)
+            {
+                rb.freezeRotation = true;
+            }
+        }
+    }
+
+
+    IEnumerator Launch()
+    {
+        foreach (Rigidbody rb in ragdollRigidBodies)
+        {
+            rb.AddForce(-transform.forward * Random.Range(-5.0f, 20.0f), ForceMode.Impulse);
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        transform.position = ragdollRigidBodies[0].position;
+        foreach (Rigidbody rb in ragdollRigidBodies)
+        {
+            rb.position = Vector3.zero;
+        }
     }
 }
