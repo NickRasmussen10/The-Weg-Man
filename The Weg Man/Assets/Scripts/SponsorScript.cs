e9d949aa964e5c6eb70fc6b0729eb471b42e82c0
Nick <nickras35@gmail.com>
2020-02-08 20:51:04 -0500

    technically sponsored by wegmans

---
+++ The Weg Man/Assets/Scripts/SponsorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SponsorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Stall());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Stall()
+    {
+        yield return new WaitForSeconds(8.0f);
+        SceneManager.LoadScene(2);
+    }
+}
