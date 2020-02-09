a6348f082c35b60500a2fe7c717fc3eb49456a53
kfm4772 <kfm4772@rit.edu>
2020-02-08 23:46:07 -0500

    Resized prefabs

--- The Weg Man/Assets/Scripts/Timer.cs
+++ The Weg Man/Assets/Scripts/Timer.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeLeft;
+    public float timeLeft;  // assign the amount of time in the editor
     public Text timerText;
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime;
-        timerText.text = timeLeft.ToString();
+        timeLeft -= Time.deltaTime; // decrease the amount of time left
+        timerText.text = "Time: " + timeLeft.ToString("F0");    // cast timeLeft to string with no decimal
     }
 }
