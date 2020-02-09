2e97e726951c5a45233aecc3e00fc6620f26431d
kfm4772 <kfm4772@rit.edu>
2020-02-09 01:26:00 -0500

    Added voice acting

---
+++ The Weg Man/Assets/Scripts/ShelfCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfCollide : MonoBehaviour
+{
+    public AudioSource soundEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("play sound");
+        //if(other.gameObject.tag == "WegMan" && shoppingList.Contains(other.gameObject))
+        if (other.gameObject.tag == "WegMan")
+        {
+            
+            soundEffect.Play();
+        }
+    }
+}
